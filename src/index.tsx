275b0db9a61980ed8c1e77f4610a9b3d153e15bd
Prashant <pr.shinde@easternenterprise.com>
2021-09-29 15:38:03 +0530

    Added saga

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from 'redux-saga';
 
 import App from "./App/App";
 import reducers from "reducers";
-
+import todoSaga from "sagas/todoSaga";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const store = createStore(reducers, composeWithDevTools(applyMiddleware()));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(todoSaga);
 
 ReactDOM.render(
 	<React.StrictMode>
